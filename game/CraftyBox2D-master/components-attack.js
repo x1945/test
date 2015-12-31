8d7d195b0abb1b636d5f8ed9aa35820e097a92bc
Fantasy-VAIO <x1945@pchoem.com.tw>
2015-12-31 18:38:37 +0800

    雛形

--- game/CraftyBox2D-master/components-attack.js
+++ game/CraftyBox2D-master/components-attack.js
@@ -1,4 +1,48 @@
 var attackStepSecond = 50;
+
+// 魔法陣
+Crafty.c("attackHexagram", {
+	init : function() {
+		this.addComponent("2D, Canvas, SpriteAnimation, Hexagram");
+		this.origin("center");
+		var s = 100;
+		var iceArray = [ [ 0, 2 ], [ 1, 2 ], [ 2, 2 ] ];
+		this.reel("ice", iceArray.length * s, iceArray);
+		var fireArray = [ [ 3, 2 ], [ 4, 2 ], [ 5, 2 ] ];
+		this.reel("wind", fireArray.length * s, fireArray);
+		var windArray = [ [ 6, 2 ], [ 7, 2 ], [ 8, 2 ] ];
+		this.reel("fire", windArray.length * s, windArray);
+		var lightArray = [ [ 9, 2 ], [ 10, 2 ], [ 11, 2 ] ];
+		this.reel("light", lightArray.length * s, lightArray);
+		// var darkArray = [ [ 6, 4 ], [ 7, 4 ], [ 8, 4 ], [ 6, 5 ], [ 7, 5 ],
+		// [ 8, 5 ], [ 6, 6 ], [ 7, 6 ], [ 8, 6 ], [ 6, 7 ], [ 7, 7 ],
+		// [ 8, 7 ] ];
+		var darkArray = [ [ 6, 6 ], [ 7, 6 ], [ 8, 6 ] ];
+		this.reel("dark", darkArray.length * s, darkArray);
+
+	},
+	play : function(name) {
+		this.animate(name, -1);
+		return this;
+	},
+	count : 0,
+	events : {
+		"EnterFrame" : function() {
+			// this.body.SetAngularVelocity(20);
+			// this.body.ApplyTorque(new b2Vec2(10, 0));
+			// this.body.ApplyAngularImpulse( 100 );
+			// this.body.rotation ++;
+
+			// 飛出邊界
+			if (this.x > 320 || this.x < -20 || this.y > 420 || this.y < -20) {
+				// console.log('attackHexagram destory');
+				world.DestroyBody(this.body);
+				this.destroy();
+			}
+		}
+	}
+});
+
 // main
 Crafty.c("attackAnimation", {
 	init : function() {
@@ -44,8 +88,11 @@ Crafty.c("exeAttack2", {
 Crafty.c("exeAttack3", {
 	init : function() {
 		this.addComponent("attackAnimation");
+		// this.setAnimation("Attack3", [ [ 0, 0 ], [ 1, 0 ], [ 2, 0 ], [ 3, 0
+		// ],
+		// [ 4, 0 ], [ 0, 1 ], [ 1, 1 ], [ 2, 1 ], [ 3, 1 ], [ 4, 1 ] ]);
 		this.setAnimation("Attack3", [ [ 0, 0 ], [ 1, 0 ], [ 2, 0 ], [ 3, 0 ],
-				[ 4, 0 ], [ 0, 1 ], [ 1, 1 ], [ 2, 1 ], [ 3, 1 ], [ 4, 1 ] ]);
+				[ 4, 0 ], [ 0, 1 ], [ 1, 1 ], [ 2, 1 ] ]);
 		this.playAnimation();
 	}
 });
@@ -71,12 +118,12 @@ Crafty.c("exeAttack5", {
 Crafty.c("exeMeteor", {
 	init : function() {
 		this.addComponent("attackAnimation");
-		// this.setAnimation("Meteor", [ [ 0, 3 ], [ 1, 3 ], [ 2, 3 ], [ 3, 3 ],
-		// [ 4, 3 ], [ 0, 4 ], [ 1, 4 ] ]);
-		this.setAnimation("Meteor", [ [ 3, 0 ], [ 4, 0 ], [ 0, 1 ], [ 1, 1 ],
-				[ 2, 1 ], [ 3, 1 ], [ 4, 1 ], [ 0, 2 ], [ 1, 2 ], [ 2, 2 ],
-				[ 3, 2 ], [ 4, 2 ], [ 0, 3 ], [ 1, 3 ], [ 2, 3 ], [ 3, 3 ],
+		this.setAnimation("Meteor", [ [ 0, 3 ], [ 1, 3 ], [ 2, 3 ], [ 3, 3 ],
 				[ 4, 3 ], [ 0, 4 ], [ 1, 4 ] ]);
+		// this.setAnimation("Meteor", [ [ 3, 0 ], [ 4, 0 ], [ 0, 1 ], [ 1, 1 ],
+		// [ 2, 1 ], [ 3, 1 ], [ 4, 1 ], [ 0, 2 ], [ 1, 2 ], [ 2, 2 ],
+		// [ 3, 2 ], [ 4, 2 ], [ 0, 3 ], [ 1, 3 ], [ 2, 3 ], [ 3, 3 ],
+		// [ 4, 3 ], [ 0, 4 ], [ 1, 4 ] ]);
 		this.playAnimation();
 	}
 });
@@ -86,6 +133,22 @@ Crafty.c("exeDarkness1", {
 		this.addComponent("attackAnimation");
 		this.setAnimation("Darkness1", [ [ 0, 0 ], [ 1, 0 ], [ 2, 0 ],
 				[ 3, 0 ], [ 4, 0 ], [ 0, 1 ], [ 1, 1 ], [ 2, 1 ], [ 3, 1 ] ]);
+		this.playAnimation();
+	}
+});
+
+Crafty.c("exeIce3", {
+	init : function() {
+		this.addComponent("attackAnimation");
+		this.setAnimation("Ice3", [ [ 0, 0 ], [ 1, 0 ], [ 2, 0 ], [ 3, 0 ] ]);
+		this.playAnimation();
+	}
+});
+
+Crafty.c("exeIce32", {
+	init : function() {
+		this.addComponent("attackAnimation");
+		this.setAnimation("Ice3", [ [ 3, 1 ], [ 4, 1 ], [ 0, 2 ] ]);
 		this.playAnimation();
 	}
 });
--- game/CraftyBox2D-master/demo3.js
+++ game/CraftyBox2D-master/demo3.js
@@ -2,6 +2,7 @@ var size = 50;
 var zIndex = 8;
 var world, PTM_RATIO = 32, w = size * 6, h = size * 8, hw = w / 2, hh = h / 2, mouseJoint, ctx = this, isMouseDown = false, mouseX, mouseY, mousePVec, selectedBody, selectedFixture;
 var objects = [];
+var hero = [];
 window.onload = function() {
 	gameInit();
 };
@@ -55,15 +56,20 @@ gameInit = function() {
 
 	// characteristic
 	Crafty.sprite(24, "img/IconSet.png", {
-		characteristic0 : [ 8, 6 ],
-		characteristic1 : [ 9, 6 ],
-		characteristic2 : [ 10, 6 ],
-		characteristic3 : [ 11, 6 ],
-		characteristic4 : [ 12, 6 ],
-		characteristic5 : [ 13, 6 ],
-		characteristic6 : [ 14, 6 ],
-		characteristic7 : [ 15, 6 ],
-		characteristic8 : [ 16, 6 ]
+		characteristic0 : [ 8, 11 ],
+		characteristic1 : [ 9, 11 ],
+		characteristic2 : [ 10, 11 ],
+		characteristic3 : [ 11, 11 ],
+		characteristic4 : [ 12, 11 ],
+		characteristic5 : [ 13, 11 ],
+		characteristic6 : [ 14, 11 ],
+		characteristic7 : [ 15, 11 ],
+		characteristic8 : [ 16, 11 ]
+	});
+
+	// Hexagram
+	Crafty.sprite(32, "img/Hexagram.png", {
+		Hexagram : [ 0, 0 ]
 	});
 
 	// status
@@ -107,16 +113,22 @@ gameInit = function() {
 	Crafty.sprite(192, "Animations/Meteor.png", {
 		Meteor : [ 0, 0 ]
 	});
+	// ice3
+	Crafty.sprite(192, "Animations/Ice3.png", {
+		Ice3 : [ 0, 0 ]
+	});
 
 	Crafty.scene("loading", function() {
 		Crafty.load({
 			images : [ "img/822.png", "img/912.png", "img/945.png",
 					"img/1025.png", "img/1026.png", "img/1046.png",
 					"img/1047.png", "img/1048.png", "img/IconSet.png",
+					"img/Hexagram.png", "img/DarkSpace1.png",
 					"Animations/Attack1.png", "Animations/Attack2.png",
 					"Animations/Attack3.png", "Animations/Attack4.png",
 					"Animations/Attack5.png", "Animations/Darkness1.png",
-					"Animations/Meteor.png", "Animations/Balloon.png" ],
+					"Animations/Meteor.png", "Animations/Ice3.png",
+					"Animations/Balloon.png" ],
 			audio : {
 				"theme" : [ "audio/theme2.mp3" ],
 				"Explosion" : [ "audio/Explosion1.ogg" ],
@@ -124,6 +136,9 @@ gameInit = function() {
 			}
 		}, function() {
 			// Crafty.audio.play("theme", -1);
+			// '#FFFFFF url(img/DarkSpace1.png) no-repeat center center';
+			// var bbb = 'url(img/DarkSpace1.png) no-repeat';
+			// Crafty.background(bbb);
 			console.log('load finish');
 			Crafty.scene("main");
 		});
@@ -147,6 +162,7 @@ gameInit = function() {
 }
 
 generateWorld = function() {
+	var thickness = 50;
 	// 邊牆
 	// density : 0, // 1.0 質量[旋轉](設置密度密度為0，即表示該剛體是靜止不動的)
 	// friction : 0, // 2 表面摩擦力
@@ -160,6 +176,7 @@ generateWorld = function() {
 		friction : 10,
 		restitution : 0,
 		shape : [ [ 0, 0 ], [ w, 0 ] ]
+	// shape : [ [ 0, -thickness ], [ w, -thickness ], [ w, 0 ], [ 0, 0 ] ]
 	});
 	walls.addFixture({
 		bodyType : 'static',
@@ -167,6 +184,7 @@ generateWorld = function() {
 		friction : 10,
 		restitution : 0,
 		shape : [ [ 0, 0 ], [ 0, h ] ]
+	// shape : [ [ -thickness, 0 ], [ -thickness, h ], [ 0, h ], [ 0, 0 ] ]
 	});
 	walls.addFixture({
 		bodyType : 'static',
@@ -174,6 +192,8 @@ generateWorld = function() {
 		friction : 10,
 		restitution : 0,
 		shape : [ [ w, 0 ], [ w, h ] ]
+	// shape : [ [ w, 0 ], [ w, h ], [ w + thickness, h ],
+	// [ w + thickness, 0 ] ]
 	});
 	walls.addFixture({
 		bodyType : 'static',
@@ -181,12 +201,16 @@ generateWorld = function() {
 		friction : 10,
 		restitution : 0,
 		shape : [ [ 0, h ], [ w, h ] ]
+	// shape : [ [ 0, h ], [ w, h ], [ w, h + thickness ],
+	// [ 0, h + thickness ] ]
 	});
 
 	// 建置角色
 	genterateChars();
 	// 建置感應區
 	createSense();
+	// 測試區
+	// testCreate();
 }
 
 var moveArea = [];
@@ -238,12 +262,17 @@ var ContactSense = function(data) {
 			// 和角色交換才播音效
 			if (block.body.GetUserData().has("since")) {
 				Crafty.audio.play("switch");
+				var attack = Crafty.e("exeIce3");
+				attack.attr({
+					x : data.x + size / 2 - attack.w / 2,
+					y : data.y + size / 2 - attack.h / 2,
+					z : selectedBody.GetUserData().z - 1
+				});
 			} else {
-				var n = Crafty.math.randomInt(1, 5);
+				// var n = Crafty.math.randomInt(1, 5);
 				// var attack = Crafty.e("exeAttack" + n);
 				// var attack = Crafty.e("exeMeteor");
-				var attack = Crafty.e("exeDarkness1");
-
+				var attack = Crafty.e("exeIce32");
 				attack.attr({
 					x : data.x + size / 2 - attack.w / 2,
 					y : data.y + size / 2 - attack.h / 2,
@@ -273,7 +302,7 @@ genterateChars = function() {
 		var role = "2D, Canvas, Mouse, Box2D, since, Sense, " + "role"
 				+ (i + 1);
 		if (hasD) {
-			role = "2D, Canvas, Mouse, Box2D, role" + (i + 1);
+			role = "2D, Canvas, Mouse, Box2D, enemy, role" + (i + 1);
 			shape = "box";
 		}
 		// var role = "2D, Canvas, Mouse, Box2D, since";
@@ -316,7 +345,7 @@ genterateChars = function() {
 		fallingElement.attach(Crafty.e('Square').attr({
 			x : (i % 6) * size - 2,
 			y : i * size - 2, // 0
-		}).color(hasD ? 'rgba(255, 0, 0, 0.3)' : 'rgba(0, 0, 255, 0.3)'));
+		}).color(hasD ? 'rgba(255, 0, 0, 0.5)' : 'rgba(0, 0, 255, 0.5)'));
 
 		// characteristic
 		fallingElement.attach(Crafty.e('icon').set('characteristic' + i).attr({
@@ -327,16 +356,22 @@ genterateChars = function() {
 		}));
 
 		// status
-		var status = Crafty.e("2D, Canvas, SpriteAnimation, status" + i);
-		status.attr({
-			x : (i % 6) * size + 20,
-			y : i * size - 32,
-		});
-		status.reel("statusAction", 1000, [ [ 1, i ], [ 2, i ], [ 3, i ],
-				[ 4, i ], [ 5, i ], [ 6, i ], [ 7, i ] ]);
-		status.animate("statusAction", -1);
-		fallingElement.attach(status);
+		// var status = Crafty.e("2D, Canvas, SpriteAnimation, status" + i);
+		// status.attr({
+		// x : (i % 6) * size + 20,
+		// y : i * size - 32,
+		// });
+		// status.reel("statusAction", 1000, [ [ 1, i ], [ 2, i ], [ 3, i ],
+		// [ 4, i ], [ 5, i ], [ 6, i ], [ 7, i ] ]);
+		// status.animate("statusAction", -1);
+		// fallingElement.attach(status);
 
+		// 我方
+		if (hasD) {
+			// hero.push();
+		} else {
+			hero.push(fallingElement);
+		}
 		// 放入二維陣列
 		objects[(i % 6)][i] = fallingElement;
 	}
@@ -354,6 +389,8 @@ genterateChars = function() {
 		mouseY = undefined;
 
 		if (selectedBody != null) {
+			var attackPoint = Crafty.clone(finalMovePoint);
+
 			var userData = selectedBody.GetUserData()
 			userData.p = Crafty.clone(finalMovePoint);
 
@@ -365,6 +402,12 @@ genterateChars = function() {
 
 			selectedFixture.SetSensor(true);
 			selectedFixture = null;
+
+			// attack
+			// attackCreate(attackPoint);
+			for ( var i in hero) {
+				attackCreate(hero[i].p);
+			}
 		}
 	});
 
@@ -567,4 +610,150 @@ function setPosition(body, p) {
 			y : p.y * size / PTM_RATIO
 		});
 	}
+}
+
+var vec4 = [ new b2Vec2(30, 0), new b2Vec2(-30, 0), new b2Vec2(0, 30),
+		new b2Vec2(0, -30) ];
+var vec41 = [ new b2Vec2(20, -20), new b2Vec2(-20, -20), new b2Vec2(20, 20),
+		new b2Vec2(-20, 20) ];
+var vec8 = [ new b2Vec2(20, 0), new b2Vec2(-20, 0), new b2Vec2(0, 20),
+		new b2Vec2(0, -20), new b2Vec2(20, -20), new b2Vec2(-20, -20),
+		new b2Vec2(20, 20), new b2Vec2(-20, 20) ];
+
+var HexagramName = [ 'ice', 'wind', 'fire', 'light', 'dark' ];
+var HexagramName2 = [ 'exeAttack3', 'exeAttack5', 'exeAttack2', 'exeAttack4',
+		'exeDarkness1' ];
+var attackCreate = function(p) {
+	var hexagramIndex = Crafty.math.randomInt(0, 4);
+
+	for (var i = 0; i <= 3; i++) {
+		// var attack = Crafty.e('attackHexagram').play('dark');
+		var attack = Crafty.e('attackHexagram').play(
+				HexagramName[hexagramIndex]);
+		attack.addComponent("Box2D");
+		attack.addComponent("Collision");
+		attack.attr({
+			attackIndex : hexagramIndex,
+			p : Crafty.clone(p),
+			x : p.x * size + 8,
+			y : p.y * size + 8,
+			// h : size,
+			// w : size,
+			r : 0.51
+		}).box2d({
+			isSensor : true, // 傳感器(default:true, 碰撞時反饋
+			density : 0, // 1.0 質量[旋轉](設置密度密度為0，即表示該剛體是靜止不動的)
+			friction : 0, // 2 表面摩擦力
+			restitution : 0, // 0.2 表面張力[彈力](這個值越大，剛體越硬) 彈力,
+			bodyType : 'dynamic', // dynamic
+			shape : 'circle'
+		});
+		// .onContact("enemy", ContactEnemy);
+
+		// console.log('boxAttack:', boxAttack);
+		// var attackBody = boxAttack.GetBody();
+		// var attackBody = attack.body;
+		// attack.body.SetLinearVelocity(new b2Vec2(50, 0));
+		// console.log(attack.body);
+		attack.body.SetLinearVelocity(vec4[i]);
+		// attack.body.SetLinearVelocity(vec8[i]);
+		// attack.body.ApplyForce(vec4[i]);
+		// attack.body.SetLinearVelocity(vec41[i]);
+
+		attack.checkHits('enemy').bind(
+				"HitOn",
+				function(hitData) {
+					// console.log('HitOn >>>>:', hitData);
+					var block = hitData[0].obj
+					// var userData = this.body.GetUserData();
+					var userData = block.body.GetUserData();
+					// var attack = Crafty.e("exeDarkness1");
+					// console.log(HexagramName2[this.attackIndex]);
+					var attack = Crafty.e(HexagramName2[this.attackIndex]);
+					attack.attr({
+						x : userData.x + size / 2 - attack.w / 2,
+						y : userData.y + size / 2 - attack.h / 2,
+						z : userData.z + 100
+					});
+
+					// 傷害
+					var number = Crafty.math.randomInt(1000, 9999);
+					var dx = userData.x + 12;
+					var dy = userData.y + 12;
+					var dz = userData.z + 101;
+					block.hurt = Crafty.e("hurt").display(number, '#ff0000',
+							dx, dy, dz);
+
+					// status
+					// if (!block.has("status1")) {
+					// var ss = "2D, Canvas, SpriteAnimation, status1"
+					// var status = Crafty.e(ss);
+					// status.attr({
+					// x : userData.x + 30,
+					// y : userData.y - 32,
+					// z : userData.z + 1
+					// });
+					// status.reel("statusAction", 1000, [ [ 1, i ], [ 2, i ],
+					// [ 3, i ], [ 4, i ], [ 5, i ], [ 6, i ],
+					// [ 7, i ] ]);
+					// status.animate("statusAction", -1);
+					// block.attach(status);
+					// }
+
+					// this.resetHitChecks('piece');
+				})
+	}
+
+	// attackBody.ApplyImpulse(new b2Vec2(10 / PTM_RATIO, 0), boxAttack.body
+	// .GetWorldCenter());
+}
+
+var ContactEnemy = function(data) {
+	// console.log('Enemy>>>>:');
+	var block = data[0].obj;
+
+	var bName = block.body.GetUserData()._entityName;
+	// console.log('Enemy >>>>:', bName);
+	// var n = Crafty.math.randomInt(1, 5);
+	// var attack = Crafty.e("exeAttack" + n);
+	// var attack = Crafty.e("exeMeteor");
+	// var userData = block.body.GetUserData();
+	var userData = this.body.GetUserData();
+	var attack = Crafty.e("exeDarkness1");
+	attack.attr({
+		x : userData.x + size / 2 - attack.w / 2,
+		y : userData.y + size / 2 - attack.h / 2,
+		z : userData.z + 1
+	});
+	// Crafty.audio.play("Explosion");
+};
+
+/**
+ * 測試區
+ */
+var testCreate = function() {
+	Crafty.e('attackHexagram').attr({
+		x : 50,
+		y : 15
+	}).play('ice');
+
+	Crafty.e('attackHexagram').attr({
+		x : 100,
+		y : 15
+	}).play('fire');
+
+	Crafty.e('attackHexagram').attr({
+		x : 150,
+		y : 15
+	}).play('wind');
+
+	Crafty.e('attackHexagram').attr({
+		x : 200,
+		y : 15
+	}).play('light');
+
+	Crafty.e('attackHexagram').attr({
+		x : 250,
+		y : 15
+	}).play('dark');
 }
