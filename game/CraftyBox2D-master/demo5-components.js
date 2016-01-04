4b597c0bed05686569558a9565cc830cfdd84655
Fantasy-VAIO <x1945@pchoem.com.tw>
2016-01-04 18:33:39 +0800

    add demo5

---
+++ game/CraftyBox2D-master/demo5-components.js
@@ -0,0 +1,37 @@
+// 消失
+Crafty.c("dead", {
+	required : "2D, Canvas, Color",
+	init : function() {
+
+	},
+	toDead : function() {
+		this.dead = true;
+	},
+	count : 0,
+	dead : false,
+	events : {
+		"EnterFrame" : function() {
+			if (this.dead) {
+				if (this.count++ < 20) {
+					// this.x -= 2;
+					// this.y -= 2;
+					// this.body.x--;
+					// this.body.y--;
+					this.w += 2;
+					this.h += 2;
+					this.alpha -= 0.04;
+					this.body.SetPosition({
+						x : (this.x - 1) / PTM_RATIO,
+						y : (this.y - 1) / PTM_RATIO
+					});
+
+				} else {
+					// console.log(this);
+					world.DestroyBody(this.body);
+					this.destroy();
+					// objects[this.p.x][this.p.y] = null;
+				}
+			}
+		}
+	}
+});
